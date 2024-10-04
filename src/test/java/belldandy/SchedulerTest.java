0a1393c92034bfa139f00730bd4160129765a67f
Teletha <Teletha.T@gmail.com>
2024-10-04 19:30:26 +0900

    test

--- src/test/java/belldandy/SchedulerTest.java
+++ src/test/java/belldandy/SchedulerTest.java
@@ -96,7 +96,7 @@ class SchedulerTest extends SchedulerTestSupport {
         assert verifySuccessed(future1, "1");
         assert verifySuccessed(future2, "2");
         assert verifySuccessed(future3, "3");
-        // assert verifier1.verifyInitialDelay(50, TimeUnit.MILLISECONDS);
+        assert verifier1.verifyInitialDelay(50, TimeUnit.MILLISECONDS);
         // assert verifier1.verifyExecutionCount(1);
         // assert verifier2.verifyInitialDelay(50, TimeUnit.MILLISECONDS);
         // assert verifier2.verifyExecutionCount(1);
