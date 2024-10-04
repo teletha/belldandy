d72c552cf4e26366bfc5cf54ea6e733a429e5337
teletha <teletha@users.noreply.github.com>
2024-10-04 11:07:01 +0000

    deploy: d885a9e5cf505cf9b03beb26973bed8e5853647f

--- root.js
+++ root.js
@@ -8,13 +8,6 @@ const root = {
 		{
 			"modifiers": {
 				"#": "java.util.Collections$UnmodifiableSet",},
-			"name": "Cron",
-			"packageName": "belldandy",
-			"type": "Class"
-		},
-		{
-			"modifiers": {
-				"#": "java.util.Collections$UnmodifiableSet",},
 			"name": "Scheduler",
 			"packageName": "belldandy",
 			"type": "Class"
